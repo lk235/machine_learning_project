8e28e7dd4b1d8ad95154931677bf8196da38aa82
lk235 <cool.lk235@icloud.com>
2017-12-06 10:29:37 +0100

    12.6 day

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -20,21 +20,32 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 print len(enron_data)
 for i in enron_data.keys():
-    if 'JEFF' in i:
+    if 'LAY' in i:
         print i
 print len(enron_data['METTS MARK'])
 count = 0
 for k in enron_data:
+    # if enron_data[k]["poi"] == 1:
+    #     count = count + 1
+    # print enron_data[k]
     if enron_data[k]["poi"] == 1:
-        count = count + 1
+        if enron_data[k]['total_payments'] == 'NaN':
+            count = count + 1
 
 
-print count
+print 'total_payments',float(count)/len(enron_data)
 print enron_data['METTS MARK'].keys()
 
 print enron_data['Prentice James'.upper()]['total_stock_value']
 print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
 print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+print enron_data['SKILLING JEFFREY K'][ 'total_payments']
+print enron_data['FASTOW ANDREW S'][ 'total_payments']
+print enron_data['LAY KENNETH L'][ 'total_payments']
+
+
+
 
 
 
