8d67a60de2e37a3b2573cf4f237c6bc938f15612
lk235 <cool.lk235@icloud.com>
2017-12-08 10:22:18 +0100

    12.8 day

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,22 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    import numpy
+    predictions.np = numpy.array(predictions)
+    net_worths.np = numpy.array(net_worths)
+    diff = net_worths.np - predictions.np
+    # for i in predictions:
 
-    
+
+
     return cleaned_data
 
+import numpy
+list1 = [1,2,3,4,5]
+list2 = [2,3,4,5,7]
+np1 = numpy.array(list1)
+np2 = numpy.array(list2)
+for i in np1:
+    print i
+# print np2 - np1
+print 'OK'
